e0a320acff42955582aac79e433e935264047287
Lufann <88391810+Lufann@users.noreply.github.com>
2021-08-03 14:42:43 -0500

    Update Albergue de Perros C++ - Base.cpp

--- Albergue de Perros C++ - Base.cpp
+++ Albergue de Perros C++ - Base.cpp
@@ -229,66 +229,73 @@ void listar(){
 	limpia();
 	menu();
 }
+
 void eliminar(){
 	char cod[15];
 	int aux = 0;
 	char op,op2;
 	
 	limpia();
-	cout << "\tELIMINAR PERRO"<<endl;
-	cout << "\nIngrese codigo: ";
+	cout << "\tDESCARTAR MASCOTA"<<endl;
+	cout << "\nIngrese codigo:   ";
 	fflush(stdin);
 	fgets(cod,N,stdin);
 	cambio(cod);
 	
-	for (int i = 0; i < N && aux == 0; i++){
-		if(strcmp(cod,pe[i].codigo)	== 0){
+	for (int i = 0; i < N && aux == 0; i++)
+	{
+		if(strcmp(cod,pe[i].codigo)	== 0)
+		{
 			aux = 1;
-
 			cout << "\nNombre:           " << pe[i].nombre;
 			cout << "\nFecha de ingreso: " << pe[i].fecha;
+			cout << "\nEspecie:          " << pe[i].especie;
+			cout << "\nSexo: 		  " << pe[i].sexo;
 			cout << "\nRaza:             " << pe[i].raza;
 			cout << "\nEdad:             " << pe[i].edad;
-
 			do{
-				cout << "\n\nSeguro que desea eliminar? Pulse una tecla: <1> Si / <2> No ";
+			cout << "\n\nEsta seguro que desea eliminar esta informacion de manera permanente? Pulse una tecla: <1> Si / <2> No  ";
 				op = getch();	
 			}while (op!='1' && op!='2');
 			
-			if (op=='1'){
+			if (op=='1')
+			{
 				pe[i].lleno = 0;
 				cout << "\n\nEliminado correctamente.";
+				cout << "                                                              ";
 			}
-			if (op=='2'){
-				cout << "\n\nCodigo no eliminado. Presione una tecla para ir al menu principal..";
+			if (op=='2')
+			{
+				cout << "\n\nCodigo no eliminado. Presione una tecla para ir al menu principal ...";
 				getch();
 				limpia();
 				menu();	
 			}
 		}
+		
 	}
 	
-	if (aux == 1){
-		cout << "\n\nPresione una tecla para volver al menu principal";
+	if (aux ==1)
+	{
+		cout << "\n\nPresione una tecla para volver al menu principal ...";
 		getch();	
 		limpia();
 		menu();
 	}
-			
-	else if (aux == 0){
+					
+	else{
+ 	if (aux==0){
 		cout << "\n\n¡Codigo no encontrado!";
-		cout << "\n\nPresione una tecla para volver al menu principal..";
+		cout << "\n\nPresione una tecla para volver al menu principal ...";
 		getch();
 		limpia();
 		menu();
-	}
-	
-	cout << "\n\nPulse una tecla para ir al menu principal..";
+	}}
+	cout << "\n\nPulse una tecla para ir al menu principal ...";
 	getch();	
 	limpia();
 	menu();
 }
-
 
 
 void adopcion(){
